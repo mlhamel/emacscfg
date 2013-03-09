5a48771f555275f09fdbc5390c0f6c3ff858e284
Mathieu Leduc-Hamel <mlhamel@ajah.mlhamel.org>
2013-03-09 10:17:05 -0500

    use light mode for now

--- init.el
+++ init.el
@@ -11,6 +11,24 @@
 (defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings)
   "A list of packages to ensure are installed at launch.")
 
+(defun insert-quotes ()
+  "Inserts quotes (\") around the current region or work."
+  (interactive)
+  (let (start end bounds)
+    (if (and transient-mark-mode mark-active)
+        (setq start (region-beginning)
+              end (region-end))
+      (progn
+        (setq bounds (bounds-of-thing-at-point 'symbol))
+        (setq start (car bounds)
+              end (cdr bounds))))
+    (goto-char start)
+    (insert "\"")
+    (goto-char (+ end 1))
+    (insert "\"")))
+
+(global-set-key (kbd "M-'") 'insert-quotes)
+
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
@@ -25,7 +43,7 @@
 
 (toggle-highlight-column-when-idle 1)
 (col-highlight-set-interval 2)
-(set-face-background 'col-highlight "color-235")
+(set-face-background 'col-highlight "color-33")
 (hl-line-mode 1)
 (global-hl-line-mode 1)
 (custom-set-faces
@@ -34,7 +52,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(highlight ((t (:background "color-34" :foreground "brightwhite"))))
- '(hl-line ((t (:background "color-235")))))
+ '(hl-line ((t (:background "color-35")))))
 
 (turn-off-auto-fill)
 (remove-hook 'text-mode-hook #'turn-on-auto-fill)
